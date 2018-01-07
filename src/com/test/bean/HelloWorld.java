902a78176e72fe4a00718c825d8f9246784f3dcf
GitTest <396202374@qq.com>
2018-01-07 15:42:01 +0800

    second commit

--- src/com/test/bean/HelloWorld.java
+++ src/com/test/bean/HelloWorld.java
@@ -3,5 +3,6 @@ package com.test.bean;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("haaha");
     }
 }
